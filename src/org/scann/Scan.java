b7ca6683471117cdf3fcc56a0c7b1dec207900af
mathisankar14 <smathisankar@gmail.com>
2022-10-25 15:53:31 +0530

    my task

---
+++ src/org/scann/Scan.java
@@ -0,0 +1,13 @@
+package org.scann;
+
+import java.util.Scanner;
+
+public class Scan {
+  public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String nextLine = sc.nextLine();
+	String nextLine2 = sc.nextLine();
+	long nextLong = sc.nextLong();
+	System.out.println(nextLine+'\n'+nextLine2+'\n'+nextLong);
+}
+}
